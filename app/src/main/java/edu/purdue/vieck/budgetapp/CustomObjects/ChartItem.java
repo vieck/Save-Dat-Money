e1a5188c833cb92720ab7615b10438ca45c46b7e
Michael Vieck <vieck@purdue.edu>
2015-10-13 01:37:28 -0400

    ChartItem

---
+++ app/src/main/java/edu/purdue/vieck/budgetapp/CustomObjects/ChartItem.java
@@ -0,0 +1,27 @@
+package edu.purdue.vieck.budgetapp.CustomObjects;
+
+import android.content.Context;
+import android.view.View;
+
+import com.github.mikephil.charting.data.ChartData;
+
+/**
+ * Created by Stealth on 10/12/2015.
+ */
+public abstract class ChartItem {
+
+    protected static final int TYPE_BARCHART = 0;
+    protected static final int TYPE_LINECHART = 1;
+    protected static final int TYPE_PIECHART = 2;
+
+    protected ChartData<?> mChartData;
+
+    public ChartItem(ChartData<?> cd) {
+        this.mChartData = cd;
+    }
+
+    public abstract int getItemType();
+
+    public abstract View getView(int position, View convertView, Context c);
+
+}
