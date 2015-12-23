9c777b7525d3369ee120d02a7fe27afeaac8997f
Michael Vieck <vieck@purdue.edu>
2015-12-23 13:00:56 -0500

    Updated styles for the expandable listview

--- app/src/main/java/edu/purdue/vieck/budgetapp/Fragments/SummaryFragment.java
+++ app/src/main/java/edu/purdue/vieck/budgetapp/Fragments/SummaryFragment.java
@@ -4,14 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ExpandableListView;
-import android.widget.ListView;
 
 import edu.purdue.vieck.budgetapp.Adapters.ExpandableListViewAdapter;
 import edu.purdue.vieck.budgetapp.DatabaseHandler;
@@ -28,7 +25,7 @@ public class SummaryFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_summary, container, false);
+        View view = inflater.inflate(R.layout.fragment_data, container, false);
         mExpandableListView = (ExpandableListView) view.findViewById(R.id.data_list);
         //mExpandableListView.setLayoutManager(new LinearLayoutManager(getActivity()));
         mDataAdapter = new ExpandableListViewAdapter(getActivity(), "");
