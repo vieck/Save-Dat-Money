9fc8ed16a2e611b9220cd8f0b5cf4fe072651792
Michael Vieck <vieck@purdue.edu>
2016-01-05 16:05:42 -0500

    Creating a currency preference to store the currency value of the user

---
+++ app/src/main/java/edu/purdue/vieck/budgetapp/CustomObjects/CurrencyPreference.java
@@ -0,0 +1,10 @@
+package edu.purdue.vieck.budgetapp.CustomObjects;
+
+import android.preference.ListPreference;
+
+/**
+ * Created by Stealth on 1/5/2016.
+ */
+public class CurrencyPreference extends ListPreference {
+    
+}
