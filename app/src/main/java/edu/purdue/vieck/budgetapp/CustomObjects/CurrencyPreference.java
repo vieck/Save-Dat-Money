7f375374a9c87341bd8bee833692b2a25bfb7bff
vieck <vieck@purdue.edu>
2016-01-07 13:13:40 -0500

    Currency Preference works and doesn't repeat checked radio buttons

--- app/src/main/java/edu/purdue/vieck/budgetapp/CustomObjects/CurrencyPreference.java
+++ app/src/main/java/edu/purdue/vieck/budgetapp/CustomObjects/CurrencyPreference.java
@@ -5,7 +5,6 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.preference.ListPreference;
 import android.preference.PreferenceManager;
 import android.util.AttributeSet;
@@ -13,14 +12,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Currency;
-import java.util.List;
 import java.util.Set;
+
+import edu.purdue.vieck.budgetapp.R;
 
 /**
  * Created by Stealth on 1/5/2016.
@@ -30,15 +29,13 @@ public class CurrencyPreference extends ListPreference {
     CurrencyPreferenceAdapter mPreferenceAdapter = null;
     Context mContext;
     private LayoutInflater mInflater;
-    CharSequence[] mEntries;
-    CharSequence[] mEntryValues;
+    Currency[] mEntries;
     ArrayList<RadioButton> mButtonList;
     SharedPreferences mSharedPreferences;
     SharedPreferences.Editor mEditor;
 
 
-    public CurrencyPreference(Context context, AttributeSet attrs)
-    {
+    public CurrencyPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
         mContext = context;
         mInflater = LayoutInflater.from(context);
@@ -47,32 +44,23 @@ public class CurrencyPreference extends ListPreference {
         mEditor = mSharedPreferences.edit();
 
         Set<Currency> currencies = Currency.getAvailableCurrencies();
-        mEntries = new CharSequence[currencies.size()];
-        mEntryValues = new CharSequence[currencies.size()];
-
-        int i = 0;
-        for (Currency currency : currencies) {
-            mEntries[i] = currency.getDisplayName();
-            mEntryValues[i] = currency.getSymbol();
-        }
+        mEntries = currencies.toArray(new Currency[currencies.size()]);
     }
 
     @Override
     protected void onPrepareDialogBuilder(AlertDialog.Builder builder) {
-        super.onPrepareDialogBuilder(builder);
 
-        if (mEntries == null || mEntryValues == null || mEntries.length != mEntryValues.length )
-        {
+        int selectedPosition = mSharedPreferences.getInt("currencyPosition", -1);
+
+        if (mEntries == null) {
             throw new IllegalStateException(
                     "ListPreference requires an entries array and an entryValues array which are both the same length");
         }
 
-        mPreferenceAdapter = new CurrencyPreferenceAdapter(mContext);
+        mPreferenceAdapter = new CurrencyPreferenceAdapter(mContext, selectedPosition);
 
-        builder.setAdapter(mPreferenceAdapter, new DialogInterface.OnClickListener()
-        {
-            public void onClick(DialogInterface dialog, int which)
-            {
+        builder.setAdapter(mPreferenceAdapter, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
 
             }
         });
@@ -80,23 +68,26 @@ public class CurrencyPreference extends ListPreference {
     }
 
     private class CurrencyPreferenceAdapter extends BaseAdapter {
-        public CurrencyPreferenceAdapter(Context context) {
 
+        private int mSelection;
+
+        public CurrencyPreferenceAdapter(Context context, int mSelection) {
+            this.mSelection = mSelection;
         }
 
         @Override
         public int getCount() {
-            return 0;
+            return mEntries.length;
         }
 
         @Override
         public Object getItem(int position) {
-            return null;
+            return position;
         }
 
         @Override
         public long getItemId(int position) {
-            return 0;
+            return position;
         }
 
         @Override
@@ -104,83 +95,53 @@ public class CurrencyPreference extends ListPreference {
             View row = convertView;
             CustomHolder holder = null;
 
-            if(row == null)
-            {
-                row = mInflater.inflate(R.layout.custom_list_preference_row, parent, false);
+            if (row == null) {
+                row = mInflater.inflate(R.layout.currency_list_preference_row, parent, false);
                 holder = new CustomHolder(row, position);
                 row.setTag(holder);
+                row.setClickable(true);
+            } else {
+                holder = (CustomHolder) row.getTag();
+            }
+            holder.rButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    for (RadioButton rb : mButtonList) {
+                        if (rb.getId() != position)
+                            rb.setChecked(false);
+                    }
 
-                // do whatever you need here, for me I wanted the last item to be greyed out and unclickable
-                if(position != 3)
-                {
-                    row.setClickable(true);
-                    row.setOnClickListener(new View.OnClickListener()
-                    {
-                        public void onClick(View v)
-                        {
-                            for(RadioButton rb : mButtonList)
-                            {
-                                if(rb.getId() != position)
-                                    rb.setChecked(false);
-                            }
+                    int index = position;
+                    String currencySymbol = mEntries[index].getSymbol().toString();
+                    mEditor.putString("currencySymbol", currencySymbol);
+                    mEditor.putInt("currencyPosition", index);
+                    mEditor.commit();
 
-                            int index = position;
-                            int value = Integer.valueOf((String) mEntryValues[index]);
-                            mEditor.putInt("yourPref", value);
-
-                            Dialog mDialog = getDialog();
-                            mDialog.dismiss();
-                        }
-                    });
+                    Dialog mDialog = getDialog();
+                    mDialog.dismiss();
                 }
+            });
+            holder.text.setText(mEntries[position].getCurrencyCode() + " " + mEntries[position].getDisplayName());
+            if (mSelection == -1 && mEntries[position].getCurrencyCode().compareTo("USD") == 0) {
+                holder.rButton.setChecked(true);
+            } else if (position == mSelection) {
+                holder.rButton.setChecked(true);
+            } else {
+                holder.rButton.setChecked(false);
             }
 
             return row;
         }
 
-        class CustomHolder
-        {
+        class CustomHolder {
             private TextView text = null;
             private RadioButton rButton = null;
 
-            CustomHolder(View row, int position)
-            {
-                text = (TextView)row.findViewById(R.id.custom_list_view_row_text_view);
-                text.setText(mEntries[position]);
-                rButton = (RadioButton)row.findViewById(R.id.custom_list_view_row_radio_button);
+            CustomHolder(View row, int position) {
+                text = (TextView) row.findViewById(R.id.currency_textview);
+                rButton = (RadioButton) row.findViewById(R.id.custom_list_view_row_radio_button);
                 rButton.setId(position);
-
-                // again do whatever you need to, for me I wanted this item to be greyed out and unclickable
-                if(position == 3)
-                {
-                    text.setTextColor(Color.LTGRAY);
-                    rButton.setClickable(false);
-                }
-
-                // also need to do something to check your preference and set the right button as checked
-
                 mButtonList.add(rButton);
-                rButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
-                {
-                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
-                    {
-                        if(isChecked)
-                        {
-                            for(RadioButton rb : mButtonList)
-                            {
-                                if(rb != buttonView)
-                                    rb.setChecked(false);
-                            }
-
-                            int index = buttonView.getId();
-                            int value = Integer.valueOf((String) mEntryValues[index]);
-                            mEditor.putInt("yourPref", value);
-
-                            Dialog mDialog = getDialog();
-                            mDialog.dismiss();
-                        }
-                    }
-                });
             }
         }
     }
