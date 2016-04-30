4d72930701d4195e625497512478d986bbfa4776
vieck <vieck@purdue.edu>
2016-04-29 22:24:08 -0400

    Started backup activity

---
+++ app/src/main/java/edu/purdue/vieck/budgetapp/Activities/BackupActivity.java
@@ -0,0 +1,16 @@
+package edu.purdue.vieck.budgetapp.Activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import edu.purdue.vieck.budgetapp.R;
+
+public class BackupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_backup);
+    }
+}
